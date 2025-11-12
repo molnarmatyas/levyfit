093d9ab1cf6ccaa9f6230e8e41681236473a8899
molnarmatyas <molnarmatyas@student.elte.hu>
2025-11-12 15:41:07 +0100

    Commit part of backing stuff up

--- do_all_nevt_avg.sh
+++ do_all_nevt_avg.sh
@@ -3,18 +3,15 @@
 make clean
 make exe/onedim_EbE_or_Eavg_fit.exe
 
-nevt_avgs=(1 50 100 200 500 1000 5000 10000)
+nevt_avgs=(1 10 50 100 200 500 1000 5000 10000)
+energies=("3p0" "7p7" "27")
 
-echo "Fitting for energy 3p0"
-for avg in "${nevt_avgs[@]}"; do
-  echo "Fitting for nevt_avg: ${avg}"
-  exe/onedim_EbE_or_Eavg_fit.exe 11 "3p0" 1 10000 ${avg} 1 &> fit_log_3p0_nevtavg${avg}.log # avg. by nevt events
-done
-
-echo "Fitting for energy 27"
-for avg in "${nevt_avgs[@]}"; do
-  echo "Fitting for nevt_avg: ${avg}"
-  exe/onedim_EbE_or_Eavg_fit.exe 11 "27" 1 10000 ${avg} 1 &> fit_log_3p0_nevtavg${avg}.log # avg. by nevt events
+for energy in "${energies[@]}"; do
+  echo "Fitting for energy ${energy}"
+  for avg in "${nevt_avgs[@]}"; do
+    echo "Fitting for nevt_avg: ${avg}"
+    exe/onedim_EbE_or_Eavg_fit.exe 11 "${energy}" 1 10000 ${avg} 1 &> fit_log_${energy}_nevtavg${avg}.log # avg. by nevt events
+  done
 done
 
 echo "All done."
